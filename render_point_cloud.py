a2ca81ff2ee577a83197eb047694a843b5074e50
mazzaronecristian <>
2024-02-02 11:05:16 +0100

    Pulizia codice

--- render_point_cloud.py
+++ render_point_cloud.py
@@ -3,8 +3,26 @@ import math
 import os
 import re
 import pandas as pd
-# TODO: esegui rendering delle nuvole di punti date in input
 import sys
+
+def extract_sort_key(nome_file):
+    # Usa un'espressione regolare per estrarre tutti i numeri dal nome del file
+    numeri = re.findall(r"\d+", nome_file)
+    # Converte i numeri estratti in interi
+    return [int(numero) for numero in numeri]
+
+def load_cameras(sensor_name, camera_details):
+    cameras = camera_details.get_group(sensor_name)
+    render_cameras = []
+    for i, camera in cameras.iterrows():
+        bpy.ops.object.camera_add(location=(camera["x"],camera["y"],camera["z"]), 
+                                rotation=(math.radians(camera["x_rotation"]), math.radians(camera["y_rotation"]), math.radians(camera["z_rotation"])))
+        render_camera = bpy.context.object
+        render_camera.name = camera["name"]
+        render_camera.data.name = f"{render_camera.name}_Data"
+        render_camera.data.lens = camera["lens"]
+        render_cameras.append(render_camera)
+    return render_cameras
 
 args = sys.argv
 args = args[args.index("--") + 1 :]  # get all args after "--"
@@ -29,26 +47,6 @@ if args[-1] == "h":
 origin = args[-2].replace("\\", "/")
 destination = args[-1].replace("\\", "/")
 
-
-def extract_sort_key(nome_file):
-    # Usa un'espressione regolare per estrarre tutti i numeri dal nome del file
-    numeri = re.findall(r"\d+", nome_file)
-    # Converte i numeri estratti in interi
-    return [int(numero) for numero in numeri]
-
-def load_cameras(sensor_name, camera_details):
-    cameras = camera_details.get_group(sensor_name)
-    render_cameras = []
-    for i, camera in cameras.iterrows():
-        bpy.ops.object.camera_add(location=(camera["x"],camera["y"],camera["z"]), 
-                                rotation=(math.radians(camera["x_rotation"]), math.radians(camera["y_rotation"]), math.radians(camera["z_rotation"])))
-        render_camera = bpy.context.object
-        render_camera.name = camera["name"]
-        render_camera.data.name = f"{render_camera.name}_Data"
-        render_camera.data.lens = camera["lens"]
-        render_cameras.append(render_camera)
-    return render_cameras
-
 # * Ottieni la lista dei file nella cartella ordinati nel modo giusto
 lista_file = sorted(os.listdir(origin), key=extract_sort_key)
 
@@ -63,15 +61,8 @@ bpy.ops.object.select_all(action="SELECT")
 bpy.ops.object.delete()
 # * aggiungo un punto luce
 bpy.ops.object.lamp_add(type="POINT", location=(0, 4, 5))
-
-# * carico la camera di riferimento per il rendering
-# bpy.ops.object.camera_add(location=(0, 0, 2), 
-#                           rotation=(math.radians(90), 0, math.radians(220)))
-# render_camera = bpy.context.object
-# render_camera.name = "camera"
-# render_camera.data.name = f"{render_camera.name}_Data"
-# render_camera.data.lens = 15
-# bpy.context.scene.camera = render_camera
+bpy.context.object.name = 'Lamp'
+lamp_name = bpy.context.object.name
 
 #* genero il materiale di tipo wire da assegnare alla nuvola di punti
 mat = bpy.data.materials.new(name="Material")
@@ -86,17 +77,18 @@ camera_details = camera_details.groupby("target_sensor")
 
 
 # * carico la nuvola di punti come .ply (Stanford Polygon Library)
-for i, path in enumerate(point_clouds[0:5]):
-    #* Carico la nuvola e ottengo current_sensor
-    bpy.ops.import_mesh.ply(filepath="scans/ply/real_trajs/sensor_2_4.ply")
+for i, path in enumerate(point_clouds):
+    #* Carico la nuvola 
+    bpy.ops.import_mesh.ply(filepath=path)
     #*edita la nuvola di punti e vi assegna il materiale wire
-    break
     bpy.ops.object.editmode_toggle()
     bpy.ops.mesh.extrude_region_move()
     bpy.context.object.data.materials.append(mat)
     bpy.ops.object.editmode_toggle()
     point_cloud_name = bpy.context.object.name
     substrings = point_cloud_name.split("_")
+
+    #* ottengo current_sensor
     current_sensor = substrings[0]+"_"+substrings[1]
     if latest_sensor == None:
         render_cameras = load_cameras(current_sensor, camera_details)
@@ -104,7 +96,7 @@ for i, path in enumerate(point_clouds[0:5]):
     if latest_sensor != None and current_sensor != latest_sensor:
         #*pulisco la scena dagli oggetti usati per la precedente iterazione mantenendo la luce
         bpy.ops.object.select_all(action="SELECT")
-        bpy.data.objects['Lamp'].select = False
+        bpy.data.objects[lamp_name].select = False
         bpy.data.objects[bpy.context.object.name].select = False
         bpy.ops.object.delete()
         render_cameras = load_cameras(current_sensor, camera_details)
@@ -112,7 +104,7 @@ for i, path in enumerate(point_clouds[0:5]):
     #* Rendering
     for render_camera in render_cameras:
         bpy.context.scene.camera = render_camera
-        path = f"//image_render/{current_sensor}/{bpy.context.scene.camera.name}_{substrings[2]}.png"
+        path = f"//{destination}/{current_sensor}/{bpy.context.scene.camera.name}_{substrings[2]}.png"
         bpy.context.scene.render.filepath = path
         bpy.context.scene.render.image_settings.file_format = "PNG"
         bpy.ops.render.render(use_viewport=True, write_still=True)
@@ -123,85 +115,3 @@ for i, path in enumerate(point_clouds[0:5]):
     bpy.ops.object.select_all(action="DESELECT")
     bpy.data.objects[point_cloud_name].select = True
     bpy.ops.object.delete()
-# exit()
-
-# ? vecchio codice, prendere spunto per il nuovo
-# bpy.ops.import_mesh.ply(filepath="output.ply")
-# point_cloud = bpy.context.selected_objects[0]
-# point_cloud.location = (0, 0, 0)
-# point_cloud.rotation_euler = (0, 0, 0)
-# point_cloud.name = "point_cloud"
-
-# bpy.ops.object.editmode_toggle()
-# bpy.ops.mesh.extrude_region_move()
-# mat = bpy.data.materials.new(name="Material")
-# mat.type = "WIRE"
-# mat.emit = 10
-# point_cloud.data.materials.append(mat)
-
-# bpy.ops.object.editmode_toggle()
-
-# path = "//rendering/rendered.png"
-# bpy.context.scene.render.filepath = path
-# bpy.context.scene.render.image_settings.file_format = "PNG"
-# bpy.ops.render.render(use_viewport=True, write_still=True)
-
-# bpy.ops.object.select_all(action="SELECT")
-# bpy.ops.object.delete()
-
-# # Lista dei percorsi dei file da importare per ogni keyframe
-# percorsi_import = [
-#     "output.ply",
-#     "point_cloud.ply"
-#     # Aggiungi altri percorsi secondo necessità
-# ]
-
-# # Impostazioni comuni per l'importazione
-# import_settings = {
-#     "location": (0, 0, 0),
-#     "rotation": (0, 0, 0),
-# }
-
-# # * carica tutti i mesh presenti in percorsi_import con le impostazioni di import_settings
-# for i, path in enumerate(percorsi_import):
-#     bpy.ops.import_mesh.ply(filepath=path)
-#     mesh = bpy.context.selected_objects[0]
-#     mesh.location = import_settings["location"]
-#     mesh.rotation_euler = import_settings["rotation"]
-#     mesh.name = f"mesh_{i}"
-
-# bpy.ops.object.editmode_toggle()
-# bpy.ops.mesh.extrude_region_move(
-#     MESH_OT_extrude_region={"mirror": False},
-#     TRANSFORM_OT_translate={
-#         "value": (0, -0, 0),
-#     },
-# )
-# mat = bpy.data.materials.get("Material")
-# if mat is None:
-#     mat = bpy.data.materials.new(name="Material")
-#     mat.type = "WIRE"
-#     mat.emit = 10
-# mesh.data.materials.append(mat)
-# bpy.ops.mesh.extrude_region_move(
-#     MESH_OT_extrude_region={"mirror": False},
-#     TRANSFORM_OT_translate={
-#         "value": (-0, 0, 0),
-#         "constraint_axis": (False, False, False),
-#         "constraint_orientation": "GLOBAL",
-#         "mirror": False,
-#         "proportional": "DISABLED",
-#         "proportional_edit_falloff": "SMOOTH",
-#         "proportional_size": 1,
-#         "snap": False,
-#         "snap_target": "CLOSEST",
-#         "snap_point": (0, 0, 0),
-#         "snap_align": False,
-#         "snap_normal": (0, 0, 0),
-#         "gpencil_strokes": False,
-#         "texture_space": False,
-#         "remove_on_cancel": False,
-#         "release_confirm": False,
-#         "use_accurate": False,
-#     },
-# )
